e282ef6d1e29bcf769971e00787a85a05c13eab5
aaron-kristopher <aaron.lim.cstr@gmail.com>
2023-12-16 10:12:13 +0800

    Initialized js folder for animation

---
+++ js/animation.js
@@ -0,0 +1,27 @@
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const observer = new IntersectionObserver
+  (function(
+    entries, 
+    observer) {
+
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add('show');
+      }
+    });
+  }
+, appearOptions);
+
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach(element => {
+  observer.observe(element);
+});
+
+
